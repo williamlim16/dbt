5abf695d3335c69a50c62751dc03f605c6c28cb3
williamlim16 <williaml1601@gmail.com>
2025-07-01 11:07:48 +1000

    fix: dimension faulty

--- dbt/models/dimensions/dim_stations.sql
+++ dbt/models/dimensions/dim_stations.sql
@@ -1,15 +1,26 @@
 {{ config(materialized='table') }}
 
-WITH stations AS (
-  SELECT start_station_id as station_id, start_station_name as station_name 
-  FROM {{ ref("start_stations")}}
+WITH all_stations AS (
+  SELECT
+    start_station_id AS station_id,
+    start_station_name AS station_name
+  FROM {{ ref("start_stations") }}
 
-  UNION ALL
+  UNION ALL -- Use UNION ALL to avoid a costly deduplication step if you plan to group later
 
-  SELECT end_station_id as station_id, end_station_name as station_name
-  FROM {{ ref("end_stations")}}
-  WHERE NOT EXISTS ( SELECT * FROM {{ ref("start_stations")}} )
+  SELECT
+    end_station_id AS station_id,
+    end_station_name AS station_name
+  FROM {{ ref("end_stations") }}
+),
+
+cleaned_stations AS (
+  SELECT
+    station_id,
+    -- You can choose MIN, MAX, or a more sophisticated aggregation here
+    MIN(station_name) AS station_name
+  FROM all_stations
+  GROUP BY 1 -- Group by station_id
 )
 
-
-SELECT * FROM stations
+SELECT * FROM cleaned_stations
